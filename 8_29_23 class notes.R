c1adfb4f7e62013d38b8e3cac479daaa7feffc87
Dan Schumacher <danschumac@gmail.com>
2023-09-05 16:24:31 -0500

    moved folder to documents

---
+++ 8_29_23 class notes.R
@@ -0,0 +1,25 @@
+
+# General Notes -----------------------------------------------------------
+
+# if a doc can be opened without unzip, do that it saves memory
+# don't open CSV files in Excel. It will mess up the file!
+#   especially the date file can get messed up
+
+# IDE is a shell that provides additional functionality (integrated developer enviroment)
+# Console is where you interact with R
+# Terminal interacts with your machine
+# Bottom right window has lots of tabs
+#   file explorer
+#   Help tab
+#   packages
+#   plots
+#   presentation
+# top right has lots of tabs
+#   enviroment (tells you what is active in session)
+#   git
+#   history of commands run
+# posit IDE (R studio)
+# we will be using quarto documents
+
+# what is my working directory?
+getwd()
---
+++ README.md
@@ -0,0 +1,5 @@
+# DA6233-2023
+Visualization and Communication Class
+Taught by Dr. Ashwin Malshe
+
+added this line on desktop
